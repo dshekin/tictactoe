f6714cd00032cef8cf4af21f951c93b4373dfd42
dshekin <dshekin@ya.ru>
2013-07-21 16:22:46 +0300

    init commit
    create main class

---
+++ src/to/us/dimkas/Main.java
@@ -0,0 +1,14 @@
+package to.us.dimkas;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dimkas
+ * Date: 7/21/13
+ * Time: 12:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String arg[]) {
+
+    }
+}
