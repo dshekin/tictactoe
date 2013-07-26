40fabd5b9f9ecd725a061bbcae392f78baf74dc0
dimkas <dshekin@gmail.com>
2013-07-26 14:40:36 +0300

    Player class

---
+++ src/to/us/dimkas/HumanPlayer.java
@@ -0,0 +1,11 @@
+package to.us.dimkas;
+
+/**
+ * Created by dimkas on 26.07.13.
+ */
+public class HumanPlayer extends Player {
+    @Override
+    public void move(int x, int y) {
+
+    }
+}
---
+++ src/to/us/dimkas/Player.java
@@ -0,0 +1,10 @@
+package to.us.dimkas;
+
+/**
+ * Created by dimkas on 26.07.13.
+ */
+public abstract class Player {
+    private int type;
+
+    public abstract void move(int x, int y);
+}
